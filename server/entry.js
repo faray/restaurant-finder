9674bb1e709bf659bbac4c50b341f91b7b55c8ee
Farayola Timileyin Emmanuel <rafmme@gmail.com>
2017-12-15 23:09:47 +0100

    added dumm fies to some folder

--- server/entry.js
+++ server/entry.js
@@ -1,15 +1,17 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const logger = require('morgan')
+const path = require('path')
 const port = 3000;
 const app = express();
 
 app.use(bodyParser.urlencoded({extended:false}))
 app.use(logger('dev'))
 app.use(bodyParser.json())
+app.use(express.static('client'))
 
 app.get('/',(req,res)=>{
-	res.status(200).send({message:"Welcome"})
+	res.status(200).sendFile(path.join(__dirname,'/../template/index.html'))
 })
 
 app.all('*',(req,res)=>{
